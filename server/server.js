424f20acd7201e5c628ea2edb1a48a484168fe95
RuslanDev <Rus@Ustim-office.ft.local>
2023-09-29 14:48:36 +0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "new_schema2",
+  multipleStatements: true,
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+app.get("/", (req, res) => {
+  con.query("SELECT * FROM new_table", (err, result) => {
+    console.log(JSON.stringify(result));
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.listen(3333, () => {
+  console.log("Server started on port 3333");
+});
