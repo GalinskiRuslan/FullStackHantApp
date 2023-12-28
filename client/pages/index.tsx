54b5ef824a2f4848448dc73f55d104d6903f3908
RuslanDev <Rus@Ustim-office.ft.local>
2023-12-28 10:55:57 +0600

    change photo on main

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,16 +5,18 @@ import Image from 'next/image'
 import { Collage } from '@/components/Modules/Collage/Collage'
 import topVideo from '../public/ebbf2c5f006fe08a436d77993d6bb939.gif'
 import { Advantages } from '@/components/Modules/Advantages/Advantages'
+import { Map } from '@/components/Modules/Map/Map'
 
 export default function Home() {
   return (
     <>
       <Layout>
         <div className={cl.topcontcont}>
-          <Image alt="back" src={topVideo} />
+          <img alt="back" src="https://i.gifer.com/J4o.gif" width={1920} height={1080} />
         </div>
         <Collage />
         <Advantages />
+        <Map />
       </Layout>
     </>
   )
