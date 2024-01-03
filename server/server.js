9f6396b281f85b93374d7545537bf596c9d4a73a
RuslanDev <Rus@Ustim-office.ft.local>
2024-01-03 15:02:56 +0600

    подготовка для тестого запуска на серваке

--- server/server.js
+++ server/server.js
@@ -10,13 +10,13 @@ const db = require("./DataBase");
 
 app.use(express.json());
 app.use("/files", express.static("files"));
-app.use(corse({ credentials: true, origin: "http://localhost:3000" }));
+app.use(corse({ credentials: true, origin: "http://codeart.kz" }));
 app.use("/", auth, category, vacansy, response);
 
 const serverStart = () => {
   try {
-    app.listen(3333, () => {
-      console.log("Server started on port 3333");
+    app.listen(6969, () => {
+      console.log("Server started on port 6969");
     });
   } catch (error) {
     console.log(error);
